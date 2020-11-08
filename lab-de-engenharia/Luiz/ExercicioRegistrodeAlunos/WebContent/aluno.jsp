e110d9278aca73dc3c27da4b5dcfd9b573bd1c45
lfernando488 <lfernando488@gmail.com>
2020-11-08 01:06:57 -0300

    registro de alunos

---
+++ lab-de-engenharia/Luiz/ExercicioRegistrodeAlunos/WebContent/aluno.jsp
@@ -0,0 +1,55 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>ALUNO</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+	integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+
+	<div class="container">
+	<h1>Registro de alunos</h1>
+	
+	<hr>
+		<form method="post" action="./AlunoController">
+		
+		<div class="form-group">
+		<label for="id">ID:</label>
+		<input type="text" name="id" class="form-control"/> <br>
+		</div>
+		
+		<div class="form-group">
+		<label for="ra">RA:</label>
+		<input type="text" name="ra" class="form-control"/> <br>
+		</div>
+		
+		<div class="form-group">
+		<label for="nome">NOME:</label>
+		<input type="text" name="nome" class="form-control"/> <br>
+		</div>
+		
+		<div class="form-group">	
+		<label for="idade">IDADE</label>
+		<input type="text" name="idade" class="form-control"/> <br>
+		</div>
+		
+		<div class="form-group">
+		<label for="sexo">SEXO:</label>
+		<select name="sexo"  class="form-control">
+  			<option value="MASCULINO">M</option>
+  			<option value="FEMININO">F</option>
+		</select>
+		</div>
+		
+		<button type="submit" class="btn btn-primary" name="cmd" value="salvar"> ADICIONAR</button>
+		<button type="submit" class="btn btn-primary" name="cmd" value="pesquisar"> PESQUISAR</button>
+	
+	</form>
+	</div>
+	
+	
+</body>
+</html>
---
+++ lab-de-engenharia/Luiz/ExercicioRegistrodeAlunos/src/AlunoController.java
@@ -0,0 +1,65 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletContext;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/AlunoController")
+public class AlunoController extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException { 
+		
+		ServletContext app = req.getServletContext();
+		HttpSession session = req.getSession();
+		
+		List<Aluno> lista = (List<Aluno>)app.getAttribute("ALUNOS"); 
+		if(lista==null){
+			lista = new ArrayList();
+			app.setAttribute("LIVROS", lista);
+		}
+		
+		String acao = req.getParameter("cmd");
+		Aluno a = new Aluno();
+		
+		try {
+			a.setId(Long.parseLong(req.getParameter("id")));
+			a.setRa(req.getParameter("ra"));
+			a.setNome(req.getParameter("sexo"));
+			a.setIdade(Integer.parseInt(req.getParameter("idade")));
+			a.setSexo(req.getParameter("sexo"));
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		String msg = "";
+		if ("adicionar".equals(acao)) { 
+			lista.add(a);
+			msg = String.format(
+				"Aluno adicionado com sucesso, há %d alunos na lista", 
+				lista.size());
+		} else if ("pesquisar".equals(acao)) {
+			List<Aluno> localizados = new ArrayList<>();
+			for (Aluno al : lista) { 
+				if (al.getRa().contains(a.getRa())) { 
+					localizados.add(al);
+				}
+			}
+			msg = String.format(
+					"Foram localizados %d alunos com este  %s", 
+					localizados.size(), a.getRa());
+			session.setAttribute("LIVROS", localizados);
+		}
+		System.out.println(msg);
+		session.setAttribute("MENSAGEM", msg);
+		res.sendRedirect("./aluno.jsp");
+		
+	}
+	
+}
